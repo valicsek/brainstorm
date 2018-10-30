cff8290ac805d9de7e2a59098ef5e71fe7f96769
Valicsek Dávid <entriek@gmail.com>
2018-10-30 20:58:40 +0100

    Add AppRouterModule for routing

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,16 @@
+/**
+ * This file contains the routes of the app.
+ */
+import { RouterModule, Routes, Router } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const ROUTES: Routes = [
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(ROUTES) ],
+  exports: [RouterModule],
+  providers: []
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-router.module';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
